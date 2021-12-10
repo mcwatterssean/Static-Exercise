f13012cd0e9492a7ef9e37628722174835233262
Sean McWatters <mcwatterssean@gmail.com>
2021-12-10 18:33:07 -0500

    init

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+
+            Console.WriteLine($"After conversion: \r\nCelcius is {celsius}\r\nFahrenheit is {fahrenheit}");
+
+
+        }
+    }
+}
